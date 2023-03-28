7fbb0a67a6b0cc9bdf097f034b16f9a835d75885
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-03-29 00:32:45 +0300

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy, useEffect } from 'react';
-import Contacts from 'pages/Contacts';
 import { useDispatch } from 'react-redux';
-import { SharedLayout } from './SharedLayout';
 import { refreshUser } from 'redux/auth/authOperations';
+import { SharedLayout } from './SharedLayout';
+import Contacts from 'pages/Contacts';
+import { RestrictedRoute } from './RestrictedRoute';
+
 import { useAuth } from 'hooks';
 
 const Home = lazy(() => import('pages/Home'));
@@ -24,8 +26,16 @@ export function App() {
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute component={Register} redirectTo="/contacts" />
+          }
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute component={Login} redirectTo="/contacts" />}
+        />
         <Route path="/contacts" element={<Contacts />} />
         <Route path="*" element={<Home />} />
       </Route>
