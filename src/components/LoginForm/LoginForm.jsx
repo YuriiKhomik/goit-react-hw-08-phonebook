2ceef25a1bc2b174eef0828854459de8b41f03bf
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-03-29 09:50:17 +0300

    ...

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,33 +1,36 @@
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/authOperations';
 import css from './LoginForm.module.css';
+import { Field, Formik, Form } from 'formik';
+import { Button } from 'components/Button';
+
+const initialValues = {
+  email: '',
+  password: '',
+};
 
 export const LoginForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      logIn({
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(logIn(values));
+
+    resetForm();
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form className={css.form}>
+        <label className={css.label} htmlFor="email">
+          Email
+          <Field type="email" name="email" />
+        </label>
+        <label className={css.label} htmlFor="password">
+          Password
+          <Field type="password" name="password" />
+        </label>
+        <Button type="submit">Log In</Button>
+      </Form>
+    </Formik>
   );
 };
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,38 +1,40 @@
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/authOperations';
 import css from './RegisterForm.module.css';
+import { Field, Formik, Form } from 'formik';
+import { Button } from 'components/Button';
+
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+};
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      register({
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        password: form.elements.password.value,
-      })
-    );
-    form.reset();
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(register(values));
+    resetForm();
   };
 
   return (
-    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
-      <label className={css.label}>
-        Username
-        <input type="text" name="name" />
-      </label>
-      <label className={css.label}>
-        Email
-        <input type="email" name="email" />
-      </label>
-      <label className={css.label}>
-        Password
-        <input type="password" name="password" />
-      </label>
-      <button type="submit">Register</button>
-    </form>
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form className={css.form}>
+        <label className={css.label} htmlFor="name">
+          Username
+          <Field type="text" name="name" />
+        </label>
+        <label className={css.label} htmlFor="email">
+          Email
+          <Field type="email" name="email" />
+        </label>
+        <label className={css.label} htmlFor="password">
+          Password
+          <Field type="password" name="password" />
+        </label>
+        <Button type="submit">Register</Button>
+      </Form>
+    </Formik>
   );
 };
