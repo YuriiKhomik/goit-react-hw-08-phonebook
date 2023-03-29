1f70f9455bc38e59c896b16f4f59c1f17b9564f3
Yurii Khomik <yu.hamster.pl@gmail.com>
2023-03-29 15:20:50 +0300

    ...

--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { addContact } from 'redux/contacts/contactsOperations';
 import { selectContacts } from 'redux/contacts/contactsSelectors';
+import css from './PhonebookForm.module.css';
 import { Formik } from 'formik';
 import { StyledButton } from 'components/Buttons';
 import { StyledForm, Input } from './styled';
@@ -29,6 +30,7 @@ export const PhonebookForm = () => {
         <Input
           type="text"
           name="name"
+          className={css.formInput}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -37,6 +39,7 @@ export const PhonebookForm = () => {
         <Input
           type="tel"
           name="number"
+          className={css.formInput}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
